832fa7d4bb04ff74a73a50aa9f0249f88992cce6
Adam Watkins <adam@stupidpupil.co.uk>
2023-03-28 14:55:35 +0100

    Remove Origin header

--- R/read_forth_json.R
+++ R/read_forth_json.R
@@ -1,10 +1,11 @@
 read_forth_json <- function(url){
   httr::GET(url, 
     httr::add_headers(
-      Origin= "https://shop.forthwithlife.co.uk", 
-      Referer = "https://shop.forthwithlife.co.uk/"),
+    #  Origin = "https://shop.forthwithlife.co.uk", 
+      Referer = "https://shop.forthwithlife.co.uk/"
+    ),
     httr::content_type("application/json"),
-    httr::user_agent("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/110.0")
+    httr::user_agent("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:111.0) Gecko/20100101 Firefox/111.0")
   ) |>
   httr::content(as="parsed")
 }
