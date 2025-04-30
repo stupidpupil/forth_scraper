3ad5e41ffb8517f5a94d39fbf9235e35d85019f6
A Watkins <adam@stupidpupil.co.uk>
2025-04-30 19:07:37 +0100

    Fix fetching Forth products

--- R/get_forth_product_details.R
+++ R/get_forth_product_details.R
@@ -2,7 +2,7 @@ get_forth_product_details <- function(forth_product_url){
 
   tryCatch({
   handle <- forth_product_url |> 
-    stringr::str_match("health-tests/([a-z0-9_-]+/[a-z0-9_-]+)/$") |> (\(x) x[,2])()
+    stringr::str_match("health-tests/([a-z0-9_-]+/[a-z0-9_-]+)/?$") |> (\(x) x[,2])()
 
   forth_product_html <- get_html_for_url(forth_product_url)
 
@@ -19,18 +19,21 @@ get_forth_product_details <- function(forth_product_url){
   available <- TRUE # HACK
 
   biomarker_names <-  forth_product_html |> 
-    rvest::html_nodes(css=".what-gets-tested-modal .biomarker-content a") |> 
+    rvest::html_nodes(css=".what-gets-tested-buttons button") |> 
     rvest::html_text() |> unique()
 
   biomarkers <- biomarker_names |> 
     normalise_biomarker_name()
 
   venous_available <- forth_product_html |> 
-    rvest::html_node(css=".collection-methods-modal") |> rvest::html_text() |> 
-    stringr::str_detect("([Pp]hlebotomy|Superdrug|partner clinic)") |> any()
+    rvest::html_node(css=".product-usps") |> rvest::html_text() |> 
+    stringr::str_detect("[Vv]enous collection") |> any()
 
-  venous_only <- venous_available &
-    !(forth_product_html |> rvest::html_node(css=".collection-methods-modal") |> rvest::html_text() |> stringr::str_detect("Finger ?prick") |> any())
+  fingerpick_available <- forth_product_html |>
+    rvest::html_node(css=".product-usps") |> rvest::html_text() |> 
+    stringr::str_detect("[Ff]inger ?prick") |> any()
+
+  venous_only <- venous_available & !fingerpick_available
 
   list(
     title = title,
